84aca3c6eb9b8ce4f5a138f36adfb46359ae25f5
tebacho <rodolfoyanezastudillo@gmail.com>
2016-05-05 18:49:05 -0400

    estructura Demo

--- firstApp/src/cl/kibernum/firstapp/EstructurasDeControlDemo.java
+++ firstApp/src/cl/kibernum/firstapp/EstructurasDeControlDemo.java
@@ -3,7 +3,8 @@ package cl.kibernum.firstapp;
 public class EstructurasDeControlDemo {
 
     public static void main(String[] args) {
-        ifDemo();
+//        ifDemo();
+        switchDemo();
     }
     private static void ifDemo(){
         int i=12, j=11;
@@ -15,6 +16,29 @@ public class EstructurasDeControlDemo {
         if(i<j){
             System.out.println("true");
             System.out.println("false");
+        }
+    }
+    
+    private static void switchDemo(){
+        
+        int i = 100;
+        final int y = 9;
+        final int e = 100;
+        
+        switch (i){
+            
+        case y:{
+                System.out.println("0");
+                break;
+        }
+        case e:
+            System.out.println("100");
+            break;
+        case 20:
+            System.out.println("20");
+            break;
+        default:
+            System.out.println("default");
         }
     }
 }
